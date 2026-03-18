cb2752d139cee66402c7330e8130f8349ed2117a
Zian Chen <ziandev@gmail.com>
2026-03-18 13:20:39 -0400

    feat: more updates to the default view

--- src/constants/defaults.ts
+++ src/constants/defaults.ts
@@ -1,21 +1,28 @@
 export const DEFAULT_TSX =
-  `import { useState } from "react";
+`import { useState, useEffect } from "react";
 
 const Counter = () => {
   const [count, setCount] = useState<number>(0);
+
+  useEffect(() => {
+    console.log(\`Count: \${count}\`);
+  }, [count]);
+
   return <div>
     <div>{\`Counter: \${count}\`}</div>
     <button style={{
-      padding: "4px",
-      border: "1px solid #000000" 
+      border: "1px solid #000000",
+      borderRadius: "8px",
+      padding: "2px 8px",
     }} onClick={() => {setCount(count+1)}}>Add Count</button>
   </div>
 }
 
 export default function App() {
   return <div className="container">
-    <div className="welcome">Welcome to DevView</div>
-    <div className="text-3xl font-bold">Tailwind Hello World</div>
+    <div className="welcome">Welcome to Frontend Interview Sandbox</div>
+    <div className="works">CSS class works</div>
+    <div className="text-xl font-bold text-red-800">Tailwind works</div>
     <hr />
     <Counter />
   </div>
@@ -23,32 +30,35 @@ export default function App() {
 `;
 
 export const DEFAULT_CSS =
-  `/* CSS Sandbox */
-
-.welcome {
-  font-weight: bold;
-}
+`/* CSS Sandbox */
 
 .container {
   display: flex;
   flex-direction: column;
   padding: 8px;
 }
+
+.welcome {
+  font-weight: bold;
+  font-size: 24px;
+}
+
+.works {
+  font-weight: bold;
+  font-size: 24px;
+  color: red;
+}
 `;
 
 export const DEFAULT_ALGO = `// Example: sum of array
+// Function definition
 function sum(arr: number[]): number {
   let total = 0;
   for (const n of arr) total += n;
   return total;
 }
 
-// Feel free to change this entrypoint.
-function main() {
-  const result = sum([1, 2, 3, 4, 5]);
-  console.log('Sum is', result);
-}
-
-main();
+// Function execution
+const result = sum([1, 2, 3, 4, 5]);
+console.log('Sum is', result);
 `;
-
